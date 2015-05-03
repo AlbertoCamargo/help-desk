c6c93b034872ce55002ef02bf7b0031c8283d0ac
Alberto Camargo Castro <amcamargo95@gmail.com>
2015-05-03 18:34:35 -0500

    changed gemfile

--- backend-frontend/db/seeds.rb
+++ backend-frontend/db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+if Rails.env.development?
+  # Users
+  username = [950106, 931210]
+  1.upto(15) do |index|
+    user_id, password =  username[index] || Faker::Number.number(8),  username[index] || Faker::Internet.password
+    user = User.new(customer_id: user_id, full_name: Faker::Name.name, email: Faker::Internet.email, 
+                    phone: Faker::Number.number(8), rank: %w(sa usuario administrador).sample, 
+                    first_time: [true, false].sample, password: password, 
+                    password_confirmation: password )
+    user.save if user.valid?
+    user.problems.build(title: Faker::Name.title, description: )
+    user.incidents.build()
+    user.requests.build()
+  end
+end
