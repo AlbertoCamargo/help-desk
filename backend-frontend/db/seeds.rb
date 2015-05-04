cf05473da76e385f03703c24ea7a522a9bacb351
Alberto Camargo Castro <amcamargo95@gmail.com>
2015-05-04 01:18:34 -0500

    seed working 100%

--- backend-frontend/db/seeds.rb
+++ backend-frontend/db/seeds.rb
@@ -1,18 +1,21 @@
 if Rails.env.development?
-  # Users
+  
   username = [950106, 931210]
-  0.upto(15) do |index|
+  
+  0.upto(10) do |index|
     user_id, password =  username[index] || Faker::Number.number(8),  username[index] || Faker::Internet.password
     user = User.new(customer_id: user_id, full_name: Faker::Name.name, email: Faker::Internet.email, 
                     phone: Faker::Number.number(8), rank: %w(sa usuario administrador).sample, 
                     first_time: [true, false].sample, password: password, 
                     password_confirmation: password )
+  
+    # Creating problems/incovenientes/request for this user
     if user.valid?
       user.save
-      user.problems.build(title: Faker::Name.title, description: Faker::Lorem.paragraph((10..70).to_a.sample))
-      user.incidents.build(title: Faker::Name.title, description: Faker::Lorem.paragraph((10..70).to_a.sample))
-      user.requests.build(title: Faker::Name.title, description: Faker::Lorem.paragraph((10..70).to_a.sample))
-      user.save
+      rank = (0..5).to_a
+      0.upto(rank.sample) { user.problems.build(title: Faker::Name.title, description: Faker::Lorem.paragraph((10..70).to_a.sample)).save }
+      0.upto(rank.sample) { user.incidents.build(title: Faker::Name.title, description: Faker::Lorem.paragraph((10..70).to_a.sample)).save }
+      0.upto(rank.sample) { user.requests.build(title: Faker::Name.title, description: Faker::Lorem.paragraph((10..70).to_a.sample)).save }
     end
   end
 end
