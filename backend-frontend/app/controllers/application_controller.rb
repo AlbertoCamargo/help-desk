c051ba3d6500e77b3746024ed5df24c47340c490
Alberto Camargo Castro <amcamargo95@gmail.com>
2015-05-03 20:35:01 -0500

    Fix font-awesome problem

--- backend-frontend/app/controllers/application_controller.rb
+++ backend-frontend/app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :require_login
-
+  
   # Methods
   def not_authenticated
     redirect_to :login, notice: 'No estas autorizado para ralizar esta accion'
